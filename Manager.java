18cd3c97995f59cfe5d6b135d931830926bdd438
pin-nattharin <nattharin3004@gmail.com>
2025-03-03 06:37:28 +0700

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -1,5 +1,4 @@
 public class Manager extends Employee {
-    // Constructor เป็น private เพื่อป้องกันการสร้างอินสแตนซ์จากภายนอก
     public Manager(String id, String name) {
         super(id, name, RoleType.MANAGER, new ManagerAccess());
     }
